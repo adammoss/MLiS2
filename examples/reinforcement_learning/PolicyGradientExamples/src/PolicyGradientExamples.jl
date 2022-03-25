f4654df97ec7ba537627259a2024480361a9f2f9
Jamie Mair <jamie.mair@hotmail.co.uk>
2022-03-25 16:59:55 +0000

    Added a random policy

--- examples/reinforcement_learning/PolicyGradientExamples/src/PolicyGradientExamples.jl
+++ examples/reinforcement_learning/PolicyGradientExamples/src/PolicyGradientExamples.jl
@@ -1,5 +1,10 @@
 module PolicyGradientExamples
 
 # Write your package code here.
+include("environments.jl")
+include("Policies.jl")
 
+export Environments.action_space, Environments.state_space, Environments.reward, Environments.is_terminated, Environments.state, Environments.reset!, Environments.step!, Environments.create_cartpole_env
+
+export Policies.action, Policies.probability, Policies.RandomPolicy
 end
---
+++ examples/reinforcement_learning/PolicyGradientExamples/src/policies.jl
@@ -0,0 +1,21 @@
+module Policies
+using Random
+using ReinforcementLearningEnvironments
+
+
+struct RandomPolicy{T<:AbstractRNG}
+    rng::T
+end
+
+RandomPolicy() = RandomPolicy(Random.GLOBAL_RNG)
+
+action(policy::RandomPolicy, env) = rand(policy.rng, action_space(env))
+probability(::RandomPolicy, env, action) = 1.0/length(action_space(env))
+function probability(::RandomPolicy, env) 
+    a_space = action_space(env)
+    p = 1.0/length(a_space)
+    return ones(Float64, size(a_space)) .* p
+end
+
+export action, probability, RandomPolicy
+end
